aeaccd2d9e68fa1376fa5f45176d6a28c352099a
Budi Prakosa a.k.a Iyok <iyok@deadmediafm.org>
2015-05-26 10:33:43 +0700

    blink led on start

--- Attiny85SendSerial.ino
+++ Attiny85SendSerial.ino
@@ -5,7 +5,26 @@ SendOnlySoftwareSerial mySerial (0);  // Tx pin
 void setup ()
   {
   mySerial.begin(115200);
-  }
+  pinMode(PB1, OUTPUT);
+
+  digitalWrite(PB1, HIGH);
+  delay(10);  
+  digitalWrite(PB1, LOW);
+  delay(10);  
+  digitalWrite(PB1, HIGH);
+  delay(10);  
+  digitalWrite(PB1, LOW);
+  delay(10);  
+  digitalWrite(PB1, HIGH);
+  delay(10);  
+  digitalWrite(PB1, LOW);
+  delay(10);  
+  digitalWrite(PB1, HIGH);
+  delay(10);  
+  digitalWrite(PB1, LOW);
+  
+
+}
 
 
 void loop ()
@@ -13,6 +32,11 @@ void loop ()
   if (analogRead(A1) > 20)
   {
   mySerial.println ("BANG!!!!");    
+    digitalWrite(PB1, HIGH);
+    delay(25);
+  }else{
+    digitalWrite(PB1, LOW);
+ 
   }
   
   //delay (10);
